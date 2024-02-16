fc76de501d584d8ba1355a1b945946b190565e64
coli-geonwoo <kkwoo001021@naver.com>
2024-02-16 21:12:17 +0900

    feat(domain): 자동차 이동 로직 확장을 위한 인터페이스 생성

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class RacingGame {
-    private static final int RANDOM_NUMBER_RANGE = 10;
-    private static final int MOVE_BOUNDARY_NUMBER = 4;
 
     public static List<String> getWinnerNames(List<Car> cars) {
 
@@ -18,17 +16,11 @@ public class RacingGame {
                 .toList();
     }
 
-    public static void playOneRound(List<Car> cars) {
+    public static void playOneRound(List<Car> cars, MoveStrategy strategy) {
         for (Car car : cars) {
-            if (willMove()) {
+            if (strategy.willMove()) {
                 car.move();
             }
         }
-    }
-
-    //TODO 움직이는 로직의 확장성 고민해보기
-    private static boolean willMove() {
-        int randomNumber = (int) (Math.random() * RANDOM_NUMBER_RANGE);
-        return randomNumber >= MOVE_BOUNDARY_NUMBER;
     }
 }
